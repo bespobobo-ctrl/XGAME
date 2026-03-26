d935591e9b3246e6d57a8c18576898d2a8870206
GameZone <gamezone@xgame.uz>
2026-03-26 21:29:55 +0500

    fix: Allowed multiple clubs and fixed manager creation (AllowNull for telegramId)

--- server/src/database/models/User.js
+++ server/src/database/models/User.js
@@ -3,71 +3,23 @@ const sequelize = require('../../config/database');
 const bcrypt = require('bcryptjs');
 
 const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    telegramId: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-    },
-    firstName: {
-        type: DataTypes.STRING,
-    },
-    lastName: {
-        type: DataTypes.STRING,
-    },
-    username: {
-        type: DataTypes.STRING,
-    },
-    phone: {
-        type: DataTypes.STRING,
-    },
-    balance: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-    },
-    isBlocked: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: false,
-    },
-    role: {
-        type: DataTypes.STRING,
-        defaultValue: 'customer', // 'customer' | 'admin' | 'cashier' | 'super_admin'
-    },
-    password: {
-        type: DataTypes.STRING,
-    },
-    lastLoginAt: {
-        type: DataTypes.DATE,
-    },
-    lastLoginIp: {
-        type: DataTypes.STRING,
-    },
-    lastTelegramId: {
-        type: DataTypes.STRING,
-    }
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    telegramId: { type: DataTypes.STRING, unique: true, allowNull: true }, // MENEJERLAR U-N NULL BOLISHI MUMKIN
+    firstName: { type: DataTypes.STRING },
+    lastName: { type: DataTypes.STRING },
+    username: { type: DataTypes.STRING, unique: true }, // LOGIN MUST BE UNIQUE
+    phone: { type: DataTypes.STRING },
+    balance: { type: DataTypes.INTEGER, defaultValue: 0 },
+    role: { type: DataTypes.STRING, defaultValue: 'customer' }, // 'customer' | 'manager' | 'super_admin'
+    password: { type: DataTypes.STRING }, // MENEJERLAR U-N
+    ClubId: { type: DataTypes.INTEGER, allowNull: true } // MENEJER BIRIKKANI
 }, {
     hooks: {
-        beforeCreate: async (user) => {
-            if (user.password) {
-                user.password = await bcrypt.hash(user.password, 10);
-            }
-        },
-        beforeUpdate: async (user) => {
-            if (user.changed('password')) {
-                user.password = await bcrypt.hash(user.password, 10);
-            }
-        }
+        beforeCreate: async (user) => { if (user.password) user.password = await bcrypt.hash(user.password, 10); },
+        beforeUpdate: async (user) => { if (user.changed('password')) user.password = await bcrypt.hash(user.password, 10); }
     }
 });
 
-// Parolni tekshirish uchun metod
-User.prototype.comparePassword = async function (candidatePassword) {
-    if (!this.password) return false;
-    return await bcrypt.compare(candidatePassword, this.password);
-};
+User.prototype.comparePassword = async function (p) { return await bcrypt.compare(p, this.password); };
 
 module.exports = User;
